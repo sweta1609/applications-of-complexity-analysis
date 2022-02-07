d1c73ff77c879affafbdaeec95836538af47978b
sweta <52633654+sweta1609@users.noreply.github.com>
2022-02-07 15:32:37 +0530

    Create findtheuniqueelement.py

---
+++ findtheuniqueelement.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+def findUnique(arr, n):
+ 
+    XOR = 0
+    for i in range(n) :
+        XOR = XOR ^ arr[i]
+ 
+    return XOR
+    
+#taking input using fast I/O method
+def takeInput() :
+    n = int(stdin.readline().strip())
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr, n
+
+
+def printList(arr, n) : 
+    for i in range(n) :
+        print(arr[i], end = " ")
+    print()
+
+
+#main
+t = int(stdin.readline().strip())
+
+while t > 0 :
+    
+    arr, n = takeInput()
+    print(findUnique(arr, n))
+
+    t-= 1
